48fefa3bd2bdec5d861929a2dd3a916fbfb0c7ff
Kuent1 <93789245+Kuent1@users.noreply.github.com>
2024-07-02 22:32:37 +0200

    Moved whole site to public directory

--- src/scripts/error.js
+++
@@ -1,7 +0,0 @@
-// Select the try again button
-const tryAgainButton = document.getElementById('tryAgainButton');
-
-// Add event listener to redirect to index.html when the button is clicked
-tryAgainButton.addEventListener('click', () => {
-    window.location.href = 'index.html';
-});
